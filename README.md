e70eb713287078781c6f775f593565c076411ae7
José Ivan R. de Oliveira <ivan_r.o@hotmail.com>
2024-03-16 11:12:15 -0300

    feat: init db infra

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tasteease - Manage AWS RDS With Terraform
+
+```bash
+  terraform init
+  terraform plan
+  terraform apply
+```
+
---
+++ outputs.tf
@@ -0,0 +1,21 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+output "rds_hostname" {
+  description = "RDS instance hostname"
+  value       = aws_db_instance.tasteease-pgsql-rds-db.address
+  sensitive   = true
+}
+
+output "rds_port" {
+  description = "RDS instance port"
+  value       = aws_db_instance.tasteease-pgsql-rds-db.port
+  sensitive   = true
+}
+
+output "rds_username" {
+  description = "RDS instance root username"
+  value       = aws_db_instance.tasteease-pgsql-rds-db.username
+  sensitive   = true
+}
+
